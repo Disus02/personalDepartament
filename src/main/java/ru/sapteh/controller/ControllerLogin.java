56868f6c03eab23e91c9fb49e474d0842c30435d
Danil Stozhkov <lekseih2000@mail.ru>
2021-05-25 22:17:07 +0300

    BAD

--- src/main/java/ru/sapteh/controller/ControllerLogin.java
+++ src/main/java/ru/sapteh/controller/ControllerLogin.java
@@ -1,5 +1,7 @@
 package ru.sapteh.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,10 +10,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import ru.sapteh.model.Users;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ControllerLogin {
+    private final List<Users> users= new ArrayList<>();
     @FXML
     private TextField txtLogin;
     @FXML
@@ -39,5 +45,11 @@ public class ControllerLogin {
             }
 
         });
+    }
+    private void login(){
+
+    }
+    private void geUsers(){
+
     }
 }
